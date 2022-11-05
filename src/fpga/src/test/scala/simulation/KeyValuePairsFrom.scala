7a7ad68d5d5ec5761a48073e84a50b5b0fe22324
pete-restall <pete@restall.net>
2022-11-05 17:03:12 +0000

    Better .env parsing, allowing for comments and blank lines.

--- src/fpga/src/test/scala/simulation/KeyValuePairsFrom.scala
+++ src/fpga/src/test/scala/simulation/KeyValuePairsFrom.scala
@@ -1,12 +1,12 @@
 package uk.co.lophtware.msfreference.tests.simulation
 
 object KeyValuePairsFrom {
-	private val comment = """^\s*#.+$""".r
+	private val comment = """^\s*(#.*)?$""".r
 	private val keyValue = """^\s*([^=]+)\s*=\s*(.+)\s*$""".r
 
 	def apply(lines: Seq[String]): Map[String, String] = lines.map(line => line match {
-		case comment() => None
+		case comment(_) => None
 		case keyValue(key, value) => Some(key -> value)
-		case _ => throw new UnsupportedOperationException(s"Expected key=value but got unparseable; line=${line}")
+		case _ => throw new UnsupportedOperationException(s"Expected a comment, blank or key=value but got something unparseable; line=${line}")
 	}).flatten.toMap
 }
