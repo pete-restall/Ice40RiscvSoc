3f0a0f4715936577b6528112850d86f9ec63761a
pete-restall <pete@restall.net>
2022-11-05 17:51:02 +0000

    Moved the DUT stimuli into the fixture.

--- src/fpga/src/test/scala/vendor/lattice/ice40/Ice40Spram16k16Fixture.scala
+++ src/fpga/src/test/scala/vendor/lattice/ice40/Ice40Spram16k16Fixture.scala
@@ -1,6 +1,7 @@
 package uk.co.lophtware.msfreference.tests.vendor.lattice.ice40
 
 import spinal.core._
+import spinal.core.sim._
 
 import uk.co.lophtware.msfreference.tests.simulation._
 import uk.co.lophtware.msfreference.tests.vendor.lattice.ice40.spram.{SpramGiven, SpramStateMachineBuilder}
@@ -9,11 +10,17 @@ import uk.co.lophtware.msfreference.vendor.lattice.ice40.Ice40Spram16k16
 class Ice40Spram16k16Fixture extends Component {
 	val io = Ice40Spram16k16.IoBundle()
 
-	override val clockDomain = ClockDomain(clock=io.CK)
-
 	private val dut = new Ice40Spram16k16
 	io <> dut.io
 	noIoPrefix()
 
 	def given = new SpramGiven(new SpramStateMachineBuilder(io, List[Sampling => WithNextSampling]()))
+
+	def wireStimuliWithStateMachine(initialState: Sampling) = {
+		var state = initialState
+		clockDomain.withRevertedClockEdge.onSamplings { state = state.onSampling() }
+		clockDomain.forkStimulus(period=10)
+	}
+
+	override val clockDomain = ClockDomain(clock=io.CK)
 }
--- src/fpga/src/test/scala/vendor/lattice/ice40/Ice40Spram16k16Test.scala
+++ src/fpga/src/test/scala/vendor/lattice/ice40/Ice40Spram16k16Test.scala
@@ -4,7 +4,6 @@ import scala.collection.immutable.ArraySeq
 import scala.util.Random
 
 import org.scalatest.flatspec._
-import spinal.core.sim._
 
 import uk.co.lophtware.msfreference.tests.givenwhenthen._
 import uk.co.lophtware.msfreference.tests.simulation._
@@ -18,14 +17,12 @@ class Ice40Spram16k16Test extends AnyFlatSpec with SimulationFixture[Ice40Spram1
 
 	"SB_SPRAM256KA" must "be able to store 256Kib of 16-bit words" in simulator { fixture =>
 		val words = ArraySeq.fill(NumberOfWords) { Random.nextInt(1 << 16) }
-		var state = fixture
+		var test = fixture
 			.given.spramIsPoweredOn()
 			.and.populatedWith(words)
 			.when.readingFrom(address=0)
 			.then.contentsMustEqual(words)
-			.asStateMachine
 
-		fixture.clockDomain.withRevertedClockEdge.onSamplings { state = state.onSampling() }
-		fixture.clockDomain.forkStimulus(period=10)
+		fixture.wireStimuliWithStateMachine(test.asStateMachine)
 	}
 }
