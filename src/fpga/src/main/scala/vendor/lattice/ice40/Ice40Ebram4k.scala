e56c3125f17538dda11fe4f73a974caafa8c6bd4
pete-restall <pete@restall.net>
2022-11-06 18:25:49 +0000

    Simplified the EBR IO assignments.

--- src/fpga/src/main/scala/vendor/lattice/ice40/Ice40Ebram4k.scala
+++ src/fpga/src/main/scala/vendor/lattice/ice40/Ice40Ebram4k.scala
@@ -17,73 +17,33 @@ class Ice40Ebram4k(readWidth: BitCount, writeWidth: BitCount) extends Component 
 	native.io.MASK_N := io.MASK_N // TODO: FORCE MASK_N TO ALL ZERO IF writeWidth != 16
 
 	if (readWidth.value == 8 && writeWidth.value == 8) { // TODO: READ WIDTH AND WRITE WIDTH ARE INDEPENDENT
-		native.io.DI(15) := False
-		native.io.DI(14) := io.DI(7)
-		native.io.DI(13) := False
-		native.io.DI(12) := io.DI(6)
-		native.io.DI(11) := False
-		native.io.DI(10) := io.DI(5)
-		native.io.DI(9) := False
-		native.io.DI(8) := io.DI(4)
-		native.io.DI(7) := False
-		native.io.DI(6) := io.DI(3)
-		native.io.DI(5) := False
-		native.io.DI(4) := io.DI(2)
-		native.io.DI(3) := False
-		native.io.DI(2) := io.DI(1)
-		native.io.DI(1) := False
-		native.io.DI(0) := io.DI(0)
+		native.io.DI := (
+			14 -> io.DI(7),
+			12 -> io.DI(6),
+			10 -> io.DI(5),
+			8 -> io.DI(4),
+			6 -> io.DI(3),
+			4 -> io.DI(2),
+			2 -> io.DI(1),
+			0 -> io.DI(0),
+			default -> False)
 
-		io.DO(7) := native.io.DO(14)
-		io.DO(6) := native.io.DO(12)
-		io.DO(5) := native.io.DO(10)
-		io.DO(4) := native.io.DO(8)
-		io.DO(3) := native.io.DO(6)
-		io.DO(2) := native.io.DO(4)
-		io.DO(1) := native.io.DO(2)
-		io.DO(0) := native.io.DO(0)
+		io.DO := (
+			7 -> native.io.DO(14),
+			6 -> native.io.DO(12),
+			5 -> native.io.DO(10),
+			4 -> native.io.DO(8),
+			3 -> native.io.DO(6),
+			2 -> native.io.DO(4),
+			1 -> native.io.DO(2),
+			0 -> native.io.DO(0))
+
 	} else if (readWidth.value == 4 && writeWidth.value == 4) {
-		native.io.DI(15) := False
-		native.io.DI(14) := False
-		native.io.DI(13) := io.DI(3)
-		native.io.DI(12) := False
-		native.io.DI(11) := False
-		native.io.DI(10) := False
-		native.io.DI(9) := io.DI(2)
-		native.io.DI(8) := False
-		native.io.DI(7) := False
-		native.io.DI(6) := False
-		native.io.DI(5) := io.DI(1)
-		native.io.DI(4) := False
-		native.io.DI(3) := False
-		native.io.DI(2) := False
-		native.io.DI(1) := io.DI(0)
-		native.io.DI(0) := False
-
-		io.DO(3) := native.io.DO(13)
-		io.DO(2) := native.io.DO(9)
-		io.DO(1) := native.io.DO(5)
-		io.DO(0) := native.io.DO(1)
+		native.io.DI := (13 -> io.DI(3), 9 -> io.DI(2), 5 -> io.DI(1), 1 -> io.DI(0), default -> False)
+		io.DO := (3 -> native.io.DO(13), 2 -> native.io.DO(9), 1 -> native.io.DO(5), 0 -> native.io.DO(1))
 	} else if (readWidth.value == 2 && writeWidth.value == 2) {
-		native.io.DI(15) := False
-		native.io.DI(14) := False
-		native.io.DI(13) := False
-		native.io.DI(12) := False
-		native.io.DI(11) := io.DI(1)
-		native.io.DI(10) := False
-		native.io.DI(9) := False
-		native.io.DI(8) := False
-		native.io.DI(7) := False
-		native.io.DI(6) := False
-		native.io.DI(5) := False
-		native.io.DI(4) := False
-		native.io.DI(3) := io.DI(0)
-		native.io.DI(2) := False
-		native.io.DI(1) := False
-		native.io.DI(0) := False
-
-		io.DO(1) := native.io.DO(11)
-		io.DO(0) := native.io.DO(3)
+		native.io.DI := (11 -> io.DI(1), 3 -> io.DI(0), default -> False)
+		io.DO := (1 -> native.io.DO(11), 0 -> native.io.DO(3))
 	} else {
 		native.io.DI := io.DI
 		io.DO := native.io.DO
