f9a6fc83afcc477d21ad07548ed8143e697b29db
pete-restall <pete@restall.net>
2023-01-12 08:26:57 +0000

    Attempted to add RVC support but it did not work.  Parked for now.

--- src/fpga/src/test/scala/core/CpuFixture.scala
+++ src/fpga/src/test/scala/core/CpuFixture.scala
@@ -56,6 +56,9 @@ object CpuFixture {
 		val x0: Byte = 0
 		val x1: Byte = 1
 		val x2: Byte = 2
+		val x8: Byte = 8
+		val x9: Byte = 9
+		val x10: Byte = 10
 
 		def jal(rd: Byte, offset: Int) = j(0x6f, rd, offset)
 
@@ -66,12 +69,6 @@ object CpuFixture {
 			(bits(imm, 19 downto 12) << 12) |
 			((rd & 0x1f) << 7) |
 			(opcode & 0x7f)
-
-		private def bit(value: Long, bit: Int) = if ((value & (1 << bit)) != 0) 1 else 0
-
-		private def bits(value: Long, bits: Range) = (value >> bits.min) & maskFor((bits.max - bits.min) downto 0)
-
-		private def maskFor(bits: Range) = bits.foldLeft(0) { (acc, bit) => acc | (1 << bit) }
 
 		def lw(rd: Byte, rs1: Byte, offset: Int) = i(0x03, rd, rs1, offset, 2)
 
@@ -94,10 +91,73 @@ object CpuFixture {
 
 		def xori(rd: Byte, rs: Byte, imm: Int) = i(0x13, rd, rs, imm, 4)
 
+		def xor(rd: Byte, rs1: Byte, rs2: Byte) = r(0x33, rd, rs1, rs2, 4, 0)
+
+		private def r(opcode: Byte, rd: Byte, rs1: Byte, rs2: Byte, funct3: Byte, funct7: Byte) =
+			((funct7 & 0x7f) << 25) |
+			((rs2 & 0x1f) << 20) |
+			((rs1 & 0x1f) << 15) |
+			((funct3 & 0x07) << 12) |
+			((rd & 0x1f) << 7) |
+			(opcode & 0x7f)
+
 		def nop() = addi(x0, x0, 0)
 
 		def addi(rd: Byte, rs: Byte, imm: Int) = i(0x13, rd, rs, imm, 0)
 
 		def canBeReadButMustNotBeExecuted() = lw(x0, x0, -1)
+
+		def compact(instructions: CompactOpcodes => (Int, Int)) = {
+			val pair = instructions(new CompactOpcodes())
+			(pair._1.toLong & 0x0000ffffl) | ((pair._2.toLong << 16) & 0xffff0000l)
+		}
 	}
+
+	class CompactOpcodes {
+		val x8: Byte = 8
+		val x9: Byte = 9
+		val x10: Byte = 10
+
+		def lw(rd: Byte, rs1: Byte, imm: Byte) = cl(0x00, rd, rs1, imm, 2)
+
+		private def cl(op: Byte, rd: Byte, rs1: Byte, imm: Byte, funct3: Byte) =
+			((funct3 & 0x07) << 13) |
+			(bits(imm, 5 downto 3).toInt << 10) |
+			((rs1 & 0x07) << 7) |
+			(bit(imm, 2).toInt << 6) |
+			(bit(imm, 6).toInt << 5) |
+			((rd & 0x07) << 2) |
+			(op & 0x03)
+
+		def j(imm: Int) = cj(0x01, imm, 5)
+
+		private def cj(op: Byte, imm: Int, funct3: Byte) =
+			((funct3 & 0x07) << 13) |
+			(bit(imm, 11).toInt << 12) |
+			(bit(imm, 4).toInt << 11) |
+			(bits(imm, 9 downto 8).toInt << 9) |
+			(bit(imm, 10).toInt << 8) |
+			(bit(imm, 6).toInt << 7) |
+			(bit(imm, 7).toInt << 6) |
+			(bits(imm, 3 downto 1).toInt << 3) |
+			(bit(imm, 5).toInt << 2) |
+			(op & 0x03)
+
+		def addw(rd: Byte, rs2: Byte) = ca(0x01, rd, rs2, 1, 0x27)
+
+		private def ca(op: Byte, rs1: Byte, rs2: Byte, funct2: Byte, funct6: Byte) =
+			((funct6 & 0x3f) << 10) |
+			((rs1 & 0x07) << 7) |
+			((funct2 & 0x03) << 5) |
+			((rs2 & 0x07) << 2) |
+			(op & 0x03)
+
+		def nop() = ???
+	}
+
+	private def bit(value: Long, bit: Int) = if ((value & (1 << bit)) != 0) 1 else 0
+
+	private def bits(value: Long, bits: Range) = (value >> bits.min) & maskFor((bits.max - bits.min) downto 0)
+
+	private def maskFor(bits: Range) = bits.foldLeft(0) { (acc, bit) => acc | (1 << bit) }
 }
--- src/fpga/src/test/scala/core/CpuSimulationTest.scala
+++ src/fpga/src/test/scala/core/CpuSimulationTest.scala
@@ -57,7 +57,42 @@ class CpuSimulationTest extends AnyFlatSpec with LightweightSimulationFixture[Cp
 		fixture.wireStimuliUsing(test.asStateMachine)
 	}
 
-	// TODO: must be able to execute compact instructions
+	/* TODO: THIS DOES NOT CURRENTLY WORK.
+
+	I suspect it's due to the combination of enabled / disabled / switched vexriscv features, or possibly that the Wishbone adapter is hard-coded to only
+	address 32-bit words rather than 16-bit half-words.  See https://github.com/SpinalHDL/VexRiscv/issues/93#issuecomment-542762269 for some possible clues.
+	Currently parked because RVC support is not a necessity at the moment.
+
+	it must "be able to execute compact instructions" in simulator { fixture =>
+		val (tokenAddress1, tokenValue1) = (Random.between(0, 32).toByte, Random.nextLong() & 0xffffffffl)
+		val (tokenAddress2, tokenValue2) = (Random.between(0, 32).toByte, Random.nextLong() & 0xffffffffl)
+		val opcodes = fixture.opcodes
+		val test = fixture
+			.given.data(
+				tokenAddress1.toLong -> tokenValue1,
+				tokenAddress2.toLong -> tokenValue2)
+			.and.instructions(fixture.adjustedAddressesFor(
+				0l -> opcodes.xor(opcodes.x10, opcodes.x10, opcodes.x10),
+				1l -> opcodes.compact(c => (
+					c.lw(c.x8, c.x10, tokenAddress1),
+					c.j(63 * 4))
+				),
+				2l -> opcodes.canBeReadButMustNotBeExecuted(),
+
+				64l -> opcodes.compact(c => (
+					c.lw(c.x9, c.x10, tokenAddress2),
+					c.addw(c.x9, c.x8))
+				),
+				65l -> opcodes.sw(opcodes.x0, opcodes.x9, tokenAddress1 * 4),
+				66l -> opcodes.canBeReadButMustNotBeExecuted()))
+			.when.dataWrittenTo(tokenAddress1)
+			.then.dataMustEqual(
+				tokenAddress1.toLong -> ((tokenValue1 + tokenValue2) & 0xffffffffl),
+				tokenAddress2.toLong -> tokenValue2)
+
+		fixture.wireStimuliUsing(test.asStateMachine)
+	}
+	*/
 
 	// TODO: must be able to respond to interrupts
 }
